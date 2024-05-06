4335306b7af82a04220f0abb1b74f3af6f7c821d
Esteban Rojas <estered9112@gmail.com>
2024-05-06 00:12:30 -0400

    updated background color for the theme

--- lua/esteban/plugins/colorscheme.lua
+++ lua/esteban/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("tokyonight").setup({
-        style = "night",
-      })
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			local bg = "#191a2a"
+			require("tokyonight").setup({
+				style = "night",
+				on_colors = function(colors)
+					colors.bg = bg
+				end,
+			})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 }
