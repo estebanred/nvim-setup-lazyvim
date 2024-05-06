defd365ba5d8b4f3a7b26b80fdecc74f5b1f061a
Esteban Rojas <estered9112@gmail.com>
2024-05-06 10:43:50 -0400

    updated vim tree background

--- lua/esteban/plugins/nvim-tree.lua
+++ lua/esteban/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ return {
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
+		vim.cmd([[highlight NvimTreeNormal guibg=#191a2a]])
 
 		nvimtree.setup({
 			view = {
