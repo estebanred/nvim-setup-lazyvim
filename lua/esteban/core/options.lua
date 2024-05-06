dd53866baf1bcf3a932d93f0f123e09c753112ac
Esteban Rojas <estered9112@gmail.com>
2024-05-05 20:57:12 -0400

    updated lualine and added text wrap on nvim

--- lua/esteban/core/options.lua
+++ lua/esteban/core/options.lua
@@ -12,7 +12,10 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = false -- disable line wrapping
+--opt.wrap = false -- disable line wrapping
+opt.wrap = true
+opt.linebreak = true
+opt.breakat = " "
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
