f10b685ffbe8011181f875730a5ae2b9c32fc686
Esteban Rojas <estered9112@gmail.com>
2024-05-06 23:19:13 -0400

    added transparent bg for nvim

--- lua/esteban/plugins/colorscheme.lua
+++ lua/esteban/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
+local is_transparent = true
 return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			local bg = "#191a2a"
 			require("tokyonight").setup({
 				style = "night",
-				on_colors = function(colors)
-					colors.bg = bg
-				end,
+				transparent = is_transparent,
+				styles = {
+					sidebars = is_transparent and "transparent" or "dark",
+					floats = is_transparent and "transparent" or "dark",
+				},
 			})
 			vim.cmd([[colorscheme tokyonight]])
 		end,
--- lua/esteban/plugins/nvim-tree.lua
+++ lua/esteban/plugins/nvim-tree.lua
@@ -7,7 +7,7 @@ return {
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
-		vim.cmd([[highlight NvimTreeNormal guibg=#191a2a guifg=#f1eff8]])
+		vim.cmd([[highlight NvimTreeNormal guibg=NONE guifg=NONE]])
 
 		nvimtree.setup({
 			view = {
