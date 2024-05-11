bbcd6a7ce4c38f071a7c28cffdd0f0a96140de87
Esteban Rojas <estered9112@gmail.com>
2024-05-11 10:07:22 -0400

    added yank highlight to nvim

--- lua/esteban/lazy.lua
+++ lua/esteban/lazy.lua
@@ -1,22 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "esteban.plugins" }, { import = "esteban.plugins.lsp" } }, {
-    checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
+vim.api.nvim_create_augroup("highlight_yank", {})
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = "highlight_yank",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+	end,
 })
