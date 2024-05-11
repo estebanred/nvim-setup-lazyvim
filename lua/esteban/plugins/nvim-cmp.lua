cb2d0a334633b45b4c8ca925ff4a17be48391ed6
Esteban Rojas <estered9112@gmail.com>
2024-05-11 19:53:49 -0400

    added copilot to nvim

--- lua/esteban/plugins/nvim-cmp.lua
+++ lua/esteban/plugins/nvim-cmp.lua
@@ -1,5 +1,6 @@
 return {
-	"hrsh7th/nvim-cmp",
+	{ "hrsh7th/nvim-cmp" },
+	{ "github/copilot.vim" },
 	event = "InsertEnter",
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
