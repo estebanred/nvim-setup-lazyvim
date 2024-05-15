bf3f0bfa41e9496c59dcfd9f467c7671de234a55
Esteban Rojas <estered9112@gmail.com>
2024-05-14 21:51:36 -0400

    updated cmp

--- lua/esteban/plugins/colorscheme.lua
+++ lua/esteban/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
-local is_transparent = false
+local is_transparent = true
 return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			require("tokyonight").setup({
-				style = "night",
+				style = "moon",
 				transparent = is_transparent,
 				styles = {
 					sidebars = is_transparent and "transparent" or "dark",
@@ -16,3 +16,47 @@ return {
 		end,
 	},
 }
+
+-- return {
+-- 	"dgox16/oldworld.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	-- config = true,
+-- 	config = function()
+-- 		vim.cmd.colorscheme("oldworld")
+-- 	end,
+-- }
+
+-- return {
+-- 	"EdenEast/nightfox.nvim",
+--
+-- 	config = function()
+-- 		vim.cmd("colorscheme carbonfox")
+-- 	end,
+-- }
+
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+--
+-- 	config = function()
+-- 		vim.cmd("colorscheme kanagawa-dragon")
+-- 	end,
+-- }
+
+-- return {
+-- 	"nyoom-engineering/oxocarbon.nvim",
+-- 	config = function()
+-- 		vim.opt.background = "dark" -- set this to dark or light
+-- 		vim.cmd.colorscheme("oxocarbon")
+-- 	end,
+-- }
+
+-- return {
+-- 	"shaunsingh/moonlight.nvim",
+-- 	config = function()
+-- 		vim.cmd.colorscheme("moonlight") -- Set the colorscheme to moonlight
+-- 		vim.g.moonlight_contrast = true -- Enable contrast
+-- 		vim.g.moonlight_borders = false -- Disable borders
+-- 		vim.g.moonlight_disable_background = true
+-- 	end,
+-- }
